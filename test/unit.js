a619b7b659780b0a0460759dfe86d4e15f7e560f
Jörn Zaefferer <joern.zaefferer@gmail.com>
2014-10-17 14:21:35 +0200

    Build: Test

--- test/unit.js
+++ test/unit.js
@@ -1,48 +1,56 @@
-require.config({
-	paths: {
-		cldr: "../external/cldrjs/dist/cldr",
-		"cldr-data": "../external/cldr-data",
-		CLDRPluralRuleParser: "../external/CLDRPluralRuleParser/src/CLDRPluralRuleParser",
-		json: "../external/requirejs-plugins/src/json",
-		src: "../src",
-		text: "../external/requirejs-text/text"
-	}
-});
-
-require([
-
-	// core
-	"./unit/core/locale",
-
-	// date
-	"./unit/date/expand-pattern",
-	"./unit/date/timezone-hour-format",
-
-	"./unit/date/format-properties",
-	"./unit/date/parse-properties",
-	"./unit/date/tokenizer-properties",
-
-	"./unit/date/format",
-	"./unit/date/tokenizer",
-
-	"./unit/date/parse",
-
-	// message
-	"./unit/message/translate",
-
-	// number
-	"./unit/number/pattern-properties",
-	"./unit/number/format/integer-fraction-digits",
-	"./unit/number/format/significant-digits",
-	"./unit/number/format/grouping-separator",
-	"./unit/number/format-properties",
-	"./unit/number/format",
-	"./unit/number/parse-properties",
-	"./unit/number/parse",
-
-	/* plural */
-	"./unit/plural/form"
-
-], function() {
+if ( !Function.prototype.bind ) {
+	QUnit.test( "Missing es5 methods", function( assert ) {
+		assert.ok( false, "Not running any other tests, " + location.href );
+	});
 	QUnit.start();
-});
+} else {
+	require.config({
+		paths: {
+			cldr: "../external/cldrjs/dist/cldr",
+			"cldr-data": "../external/cldr-data",
+			CLDRPluralRuleParser: "../external/CLDRPluralRuleParser/src/CLDRPluralRuleParser",
+			json: "../external/requirejs-plugins/src/json",
+			src: "../src",
+			text: "../external/requirejs-text/text"
+		}
+	});
+
+	require([
+
+		// core
+		"./unit/core/locale",
+
+		// date
+		"./unit/date/expand-pattern",
+		"./unit/date/timezone-hour-format",
+
+		"./unit/date/format-properties",
+		"./unit/date/parse-properties",
+		"./unit/date/tokenizer-properties",
+
+		"./unit/date/format",
+		"./unit/date/tokenizer",
+
+		"./unit/date/parse",
+
+		// message
+		"./unit/message/translate",
+
+		// number
+		"./unit/number/pattern-properties",
+		"./unit/number/format/integer-fraction-digits",
+		"./unit/number/format/significant-digits",
+		"./unit/number/format/grouping-separator",
+		"./unit/number/format-properties",
+		"./unit/number/format",
+		"./unit/number/parse-properties",
+		"./unit/number/parse",
+
+		/* plural */
+		"./unit/plural/form"
+
+	], function() {
+		QUnit.start();
+	});
+
+}
